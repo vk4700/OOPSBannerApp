be5aca2f8819ccc71f5d9cdc7a2b1a799bc94d6d
shashankai2007-star <shashank.ai2007@gmail.com>
2026-03-20 22:08:57 +0530

    UC4

---
+++ OopsBannerApp.java
@@ -0,0 +1,24 @@
+public class OopsBannerApp{
+    public static void main(String[] args){
+        
+        String [] line = new String[7];
+        line[0]=(String.join(" " ,"   ***   ","   ***   ","******   ","  ***** "));
+        line[1]=(String.join(" "," **   ** "," **   ** ","**    ** ","**       "));
+        line[2]=(String.join(" ","**     **","**     **","**    ** "," **      "));
+        line[3]=(String.join(" ","**     **","**     **","******   ","   ***   "));
+        line[4]=(String.join(" ","**     **","**     **","**       ","      ** "));
+        line[5]=(String.join(" "," **   ** "," **   ** ","**       ","**     **"));
+        line[6]=(String.join(" ","   ***   ","   ***   ","**       ","  *****  "));
+        String [] line ={
+        (String.join(" " ,"   ***   ","   ***   ","******   ","  ***** ")),
+        (String.join(" "," **   ** "," **   ** ","**    ** ","**       ")),
+        (String.join(" ","**     **","**     **","**    ** "," **      ")),
+        (String.join(" ","**     **","**     **","******   ","   ***   ")),
+        (String.join(" ","**     **","**     **","**       ","      ** ")),
+        (String.join(" "," **   ** "," **   ** ","**       ","**     **")),
+        (String.join(" ","   ***   ","   ***   ","**       ","  *****  ")),
+        };
+        for(int i=0;i<7;i++){
+            System.out.println(line[i]);
+        }
+    }}
